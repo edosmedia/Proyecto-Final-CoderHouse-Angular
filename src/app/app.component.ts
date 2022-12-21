d9685037042db71434ca854423b42c5f4ca119a9
Eduardo Medrano <eduardomedranop@gmail.com>
2022-12-21 13:52:34 -0300

    Fix Layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'proyecto_final_angular_coderhouse';
-  
+  opened = true;
 }
